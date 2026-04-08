2c5923e38e06e416478da24f8e6ec278f970d9fe
Safeer <s.alyubary@gmail.com>
2026-04-09 01:37:29 +0400

    ran word2vec on the data

--- New-Corpus/parquet/embedding.py
+++ New-Corpus/parquet/embedding.py
@@ -8,35 +8,43 @@ import glob
 import pyarrow.parquet as pq
 
 
-files_path = "/data_files/parquet"
-
+files_path = "data_files/wiki_and_parquet"
 stopwords_file_path = "arabic_stopwords_nltk.txt"
+non_frequent_words_file_path= "data_files/data_stats/words_10_or_less_main.txt"
 model_save_path = "New-Corpus/parquet/output/word2vec.model"
 
 
-def load_stopwords(file_path):
-    with open(file_path, "r", encoding="utf-8") as f:
-        return set(f.read().split())
-
-
-def preprocess_text(text, stopwords):
-    """Clean and tokenize text specifically for Arabic root extraction."""
+def preprocess_text(text, stopwords, non_frequent_words):
+  
     text = araby.strip_tatweel(text)
     # Switched to these cleaning steps to avoid word breaking or words concatenating
-    punctuation = r'[\.،,؛;:!?؟?(){}\[\]<>"\'/\\@#$%^&*_+=~|]'
     # Remove defined punctuation
+    punctuation = r'[\.،,؛;:!?؟?(){}\[\]<>"\'/\\@#$%^&*_+=~|]'
     text = re.sub(punctuation, ' ', text)
+    # Remove Mashriki numerals (Arabic-Indic digits)
+    text = re.sub(r'[٠-٩]', ' ', text)
     # Tokenize and strip tashkeel
     tokens = tokenize(text, conditions=is_arabicrange, morphs=strip_tashkeel)
-    tokens = [token for token in tokens if token not in stopwords and len(token) > 1]
+    tokens = [t for t in tokens if t not in stopwords and t not in non_frequent_words and len(t) > 1]
     return tokens
 
 
+
+def load_stopwords(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        return set(f.read().split())
+    
+def load_non_frequent_words(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        return set(f.read().split())
+
+
 class MyCorpus:
-    def __init__(self, path, stopwords):
+    def __init__(self, path, stopwords, non_frequent_words):
         self.jsonl_files = glob.glob(os.path.join(path, "*.jsonl"))
         self.parquet_files = glob.glob(os.path.join(path, "*.parquet"))
         self.stopwords = stopwords
+        self.non_frequent_words= non_frequent_words
 
     def __iter__(self):
         for file_name in self.jsonl_files:
@@ -48,7 +56,7 @@ class MyCorpus:
                         body = data.get("Text") or data.get("text") or data.get("body") or ""
                         full_text = f"{headline} {body}"
 
-                        processed = preprocess_text(full_text, self.stopwords)
+                        processed = preprocess_text(full_text, self.stopwords, self.non_frequent_words)
                         if processed:
                             yield processed
                     except:
@@ -58,16 +66,17 @@ class MyCorpus:
             try:
                 pq_file = pq.ParquetFile(file_name)
 
-                for batch in pq_file.iter_batches(columns=["head_line", "text"], batch_size=1000):
-                    headlines = batch.column(0).to_pylist()
-                    bodies = batch.column(1).to_pylist()
+                for batch in pq_file.iter_batches(columns=["head_line", "text"], batch_size=5000):
+                    data = batch.to_pydict()
+                    headlines = data.get("head_line", [])
+                    bodies = data.get("text", [])
 
                     for headline, body in zip(headlines, bodies):
                         headline = headline if isinstance(headline, str) else ""
                         body = body if isinstance(body, str) else ""
                         full_text = f"{headline} {body}"
 
-                        processed = preprocess_text(full_text, self.stopwords)
+                        processed = preprocess_text(full_text, self.stopwords, self.non_frequent_words)
                         if processed:
                             yield processed
             except:
@@ -75,7 +84,8 @@ class MyCorpus:
 
 
 stop_words = load_stopwords(stopwords_file_path)
-sentences = MyCorpus(files_path, stop_words)
+non_frequent_words= load_non_frequent_words(non_frequent_words_file_path)
+sentences = MyCorpus(files_path, stop_words, non_frequent_words)
 
 model = Word2Vec(
     sentences=sentences,
